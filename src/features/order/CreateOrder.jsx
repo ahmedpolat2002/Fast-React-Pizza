815e0db1c1bd43bd2c67e71db9c9752a1728b725
Ahmed Polat <ahmedpolat2002@gmail.com>
2024-08-04 21:28:12 +0300

    Upload Files

---
+++ src/features/order/CreateOrder.jsx
@@ -0,0 +1,126 @@
+import { useState } from "react";
+import {
+  Form,
+  json,
+  redirect,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+
+// https://uibakery.io/regex-library/phone-number
+const isValidPhone = (str) =>
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+    str,
+  );
+
+const fakeCart = [
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetale",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
+
+function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
+
+  // const [withPriority, setWithPriority] = useState(false);
+  const cart = fakeCart;
+
+  return (
+    <div>
+      <h2>Ready to order? Let{"'"}s go!</h2>
+
+      <Form method="POST">
+        <div>
+          <label>First Name</label>
+          <input type="text" name="customer" required />
+        </div>
+
+        <div>
+          <label>Phone number</label>
+          <div>
+            <input type="tel" name="phone" required />
+          </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
+        </div>
+
+        <div>
+          <label>Address</label>
+          <div>
+            <input type="text" name="address" required />
+          </div>
+        </div>
+
+        <div>
+          <input
+            type="checkbox"
+            name="priority"
+            id="priority"
+            // value={withPriority}
+            // onChange={(e) => setWithPriority(e.target.checked)}
+          />
+          <label htmlFor="priority">Want to yo give your order priority?</label>
+        </div>
+
+        <input type="hidden" value={JSON.stringify(fakeCart)} name="cart" />
+
+        <div>
+          <button
+            disabled={isSubmitting}
+            className="rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  console.log(formData);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  const newOrder = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+
+  const errors = {};
+  if (!isValidPhone(newOrder.phone)) {
+    errors.phone = "Invalid phone number";
+  }
+  if (Object.keys(errors).length > 0) {
+    return errors;
+  }
+
+  const order = await createOrder(newOrder);
+
+  return redirect(`/order/${order.id}`);
+}
+
+export default CreateOrder;
---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,7 @@
+function UserName() {
+  return (
+    <div className="hidden text-sm font-semibold md:block">Ahmed Polat</div>
+  );
+}
+
+export default UserName;
